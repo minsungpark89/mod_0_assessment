33a73a62347b1b4723b08e11a00845ffe53ed986
Min Park <minsungpark.msp@gmail.com>
2021-06-11 19:37:45 -0600

    added values to modelB.js

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name: 'Min',
+  occupation: 'Student',
+  age: 31,
+  numberOfDogs: 1,
+  isHungry: true,
+  isYoung: false,
+  favoriteHobbies: ['climbing', 'gaming', 'skateboarding'],
+  pastJobs: ['busboy', 'chef', 'locksmith'],
+}
