f372d60a55442564bd1b36fa620bd3e3ac4403ae
Min Park <minsungpark.msp@gmail.com>
2021-06-11 19:38:40 -0600

    added variables to mdelA.js

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  name: "T.REX",
+  gender: "Male",
+  numberOfTeeth: 200,
+  numberOfFeelings: 0,
+  isMean: true,
+  isSmall: false,
+  closeFriends: ['Bob', 'Karen', 'Kyle'],
+  fewEnemies: ['Meteor', 'Ice', 'Sloth'],
+}
