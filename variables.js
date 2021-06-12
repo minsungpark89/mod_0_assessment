4dc316df0ff3bf6ad9cef552d7837f61a72ebc19
Min Park <minsungpark.msp@gmail.com>
2021-06-11 19:02:37 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,13 @@
+var movie = "Dumb and Dumber";
+var isEntertaining = true;
+movie = "Up";
+var num4 = 999;
+var num6 = 350;
+var product = num4 * num6;
+var favoriteFoods = ['pho', 'KBBQ', 'Sushi'];
+favoriteFoods.shift();
+
+//Write a line of code that prints how many characters there are in the *movie* string.
+var thing = 'movie';
+var length = text.split( '' ).filter( c => c != ' ' ).length;
+console.log( movie );
